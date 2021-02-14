8c039de806a8437227ec0243cecf42964e8de8b6
unknown <ashiqpranto@gmail.com>
2021-02-14 12:00:05 +0600

    divide & Conquer & Atcoder Education dp contest added

---
+++ Algorithms/Divide & Conquer/largest_rectangle_area_in_a_histogram.cpp
@@ -0,0 +1,139 @@
+/* � Ashiq Uddin Pranto
+   Department of Computer Science and Engineering
+   BATCH 27
+   University Of Rajshahi,Bangladesh
+*/
+#include<bits/stdc++.h>
+ 
+#define ll long long
+#define ld long double
+#define FOR(x,y) for(ll i=x;i<=y;i++)
+#define pb(x) push_back(x)
+#define mp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vii vector<int>
+#define vll vector<ll>
+#define matrix(x) vector<vector<x>>
+#define vss vector<string>
+#define PI (2*acos(0.0))
+#define sqr(x) ((x)*(x))
+#define sf(a)   scanf("%d",&a)
+#define sfl(a)  scanf("%lld",&a)
+#define sff(a,b)    scanf("%d %d",&a,&b)
+#define sffl(a,b)   scanf("%lld %lld",&a,&b)
+#define sfff(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define sfffl(a,b,c)    scanf("%lld %lld %lld",&a,&b,&c)
+#define ms(a,b) memset(a, b, sizeof(a))
+#define ff  first
+#define ss  second
+#define lcm(a, b)   ((a)*((b)/__gcd(a,b)))
+#define all(a)  a.begin(),a.end()
+#define readfile freopen("input.txt","r",stdin);
+#define writefile freopen("output.txt","w",stdout);
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define gap " "
+#define mx 104
+#define inf (ll)1e9
+#define WHITE 1
+#define GRAY 2
+#define BLACK 3
+#define EMPTY_VALUE -1
+#define mod 1000000007
+#define MOD(a,b) (a%b + b)%b
+ 
+using namespace std;
+ 
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+ll largest_rectangle_area_in_a_histogram(ll hist[], ll n)
+{
+    // Create an empty stack. The stack holds indexes  
+    // of hist[] array. The bars stored in stack are  
+    // always in increasing order of their heights. 
+    stack<ll> s; 
+  
+    ll max_area = 0; // Initialize max area 
+    ll tp;  // To store top of stack 
+    ll area_with_top; // To store area with top bar 
+                       // as the smallest bar 
+  
+    // Run through all bars of given histogram 
+    ll i = 0; 
+    while (i < n) 
+    { 
+        // If this bar is higher than the bar on top  
+        // stack, push it to stack 
+        if (s.empty() || hist[s.top()] <= hist[i]) 
+            s.push(i++); 
+  
+        // If this bar is lower than top of stack,  
+        // then calculate area of rectangle with stack  
+        // top as the smallest (or minimum height) bar.  
+        // 'i' is 'right index' for the top and element  
+        // before top in stack is 'left index' 
+        else
+        { 
+            tp = s.top();  // store the top index 
+            s.pop();  // pop the top 
+  
+            // Calculate the area with hist[tp] stack  
+            // as smallest bar 
+            area_with_top = hist[tp] * (s.empty() ? i :  
+                                   i - s.top() - 1); 
+  
+            // update max area, if needed 
+            if (max_area < area_with_top) 
+                max_area = area_with_top; 
+        } 
+    } 
+  
+    // Now pop the remaining bars from stack and calculate 
+    // area with every popped bar as the smallest bar 
+    while (s.empty() == false) 
+    { 
+        tp = s.top(); 
+        s.pop(); 
+        area_with_top = hist[tp] * (s.empty() ? i :  
+                                i - s.top() - 1); 
+  
+        if (max_area < area_with_top) 
+            max_area = area_with_top; 
+    } 
+  
+    return max_area;
+
+}
+ 
+int main()
+{
+    ll hist[] = {6,2,5,4,5,1,6};
+    ll n = sizeof(hist)/sizeof(hist[0]); 
+    cout<<"largest rectangle area in histogram is "<<largest_rectangle_area_in_a_histogram(hist,n);
+    return 0;
+}
